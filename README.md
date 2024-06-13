152b6b7197534368d97e378637ae034ebe2aecbf
Aditi <aditideshpande2908@gmail.com>
2024-06-13 18:43:33 +0530

    Initiate project

---
+++ README.md
@@ -0,0 +1,7 @@
+## Project to practise React Components and Props
+
+### REPO LINK
+
+### LIVE LINK
+
+### What I Learned?
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Anime from './components/Anime.jsx'
+import Movies from './components/Movies.jsx'
+import Shows from './components/Shows.jsx'
+
+function App() {
+	return (
+		<div>
+			<Anime />
+			<Movies />
+			<Shows />
+		</div>
+	)
+}
+
+export default App
