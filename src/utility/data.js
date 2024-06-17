b34bd6a43e98f23dc1a7558abe5723bbff238df6
Aditi <aditideshpande2908@gmail.com>
2024-06-17 13:08:50 +0530

    Add data for the project

---
+++ src/utility/data.js
@@ -0,0 +1,87 @@
+export default [
+	{
+		category: 'Anime',
+		title: 'Haikyu!!',
+		year: 2014,
+		duration: '24m',
+		episodes: 25,
+		imdbRating: 8.7,
+		description: `Determined to be like the volleyball championship's star player nicknamed "the small giant", Shoyo joins his school's volleyball club.`,
+	},
+	{
+		category: 'Anime',
+		title: 'Attack On Titan',
+		year: 2013,
+		duration: '24m',
+		episodes: 25,
+		imdbRating: 9.1,
+		description:
+			'After his hometown is destroyed, young Eren Jaeger vows to cleanse the earth of the giant humanoid Titans that have brought humanity to the brink of extinction.',
+	},
+	{
+		category: 'Anime',
+		title: 'A Silent Voice',
+		year: 2016,
+		duration: '2h 10m',
+		imdbRating: 8.1,
+		description:
+			'A young man is ostracized by his classmates after he bullies a deaf girl to the point where she moves away. Years later, he sets off on a path for redemption.',
+	},
+	{
+		category: 'Movie',
+		title: '3 idiots',
+		year: 2009,
+		duration: '2h 50m',
+		imdbRating: 8.4,
+		description:
+			'Two friends are searching for their long lost companion. They revisit their college days and recall the memories of their friend who inspired them to think differently, even as the rest of the world called them idiots',
+	},
+	{
+		category: 'Movie',
+		title: 'Begin Again',
+		year: 2013,
+		duration: '1h 44m',
+		imdbRating: 7.4,
+		description:
+			'A chance encounter between a down-and-out music business executive, and a young singer songwriter new to Manhattan, turns into a promising collaboration between the two talents.',
+	},
+	{
+		category: 'Movie',
+		title: 'Your Name Engraved Herein',
+		year: 2020,
+		duration: '1h 58m',
+		imdbRating: 7.3,
+		description:
+			'In 1987, as martial law ends in Taiwan, Jia-han and Birdy fall in love amid family pressure, homophobia and social stigma.',
+	},
+	{
+		category: 'TV Show',
+		title: 'My Liberation Diary',
+		year: 2022,
+		duration: '1h',
+		episodes: 16,
+		imdbRating: 8.2,
+		description:
+			'Three siblings, exhausted by the monotony of day-to-day adulthood, seek to find fulfillment and freedom from their humdrum lives.',
+	},
+	{
+		category: 'TV Show',
+		title: 'Dr.Kim',
+		year: 2016,
+		duration: '1h',
+		episodes: 21,
+		imdbRating: 8.4,
+		description:
+			'Romantic Doctor Kim is a real doctor story set in a small, humble hospital called Dol Dam Hospital. It is a story about people who meet Kim Sa Bu (Han Suk Kyu), a genius doctor, and discover real romance of life.',
+	},
+	{
+		category: 'TV Show',
+		title: 'Heartstopper',
+		year: 2022,
+		duration: '1h',
+		episodes: 16,
+		imdbRating: 8.6,
+		description:
+			'Teens Charlie and Nick discover their unlikely friendship might be something more as they navigate school and young love in this coming-of-age series.',
+	},
+]
